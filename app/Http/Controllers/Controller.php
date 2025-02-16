7bcc84962de730993986f007efcd9813de4d66c3
miguel000127@gmail.com <miguel000127@gmail.com>
2025-02-16 11:42:12 -0500

    [Dockerize]

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(title="API V1", version="1.0")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
